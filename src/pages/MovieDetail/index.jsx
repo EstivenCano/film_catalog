1e35b66b62becff61208b071c4747b352e4c562b
EstivenCano <estivencano99@gmail.com>
2021-05-25 21:07:32 -0500

    Movie Details Modal

--- src/pages/MovieDetail/index.jsx
+++ src/pages/MovieDetail/index.jsx
@@ -6,11 +6,21 @@ import {
   Paper,
   Typography,
   Grid,
+  CircularProgress,
+  List,
+  ListItem,
+  CardHeader,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  IconButton
 } from "@material-ui/core";
+import CancelIcon from '@material-ui/icons/Cancel';
 import useStyles from "./style";
 import axios from "axios";
 
-//TODO Create a card to MovieDetails
+//TODO Create animations to details
 // eslint-disable-next-line import/no-anonymous-default-export
 const MovieDetail = (props) => {
   const apiKey = process.env.REACT_APP_API_KEY;
@@ -35,6 +45,11 @@ const MovieDetail = (props) => {
       });
   }
 
+  function handleClick() {
+    if (movieResult.homepage !== "") {
+      window.open(movieResult.homepage, "_blank");
+    }
+  }
   useEffect(() => {
     getMovieById();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,17 +66,38 @@ const MovieDetail = (props) => {
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
         >
-          <Container component="main" maxWidth="xs">
+          <Container className={classes.principalGrid}>
             <CssBaseline />
             <div className={classes.root}>
               <Paper className={classes.paper} elevation={3}>
                 <Grid container>
-                  <Grid xs={12} md={7} item>
+                  <Grid xs={12} md={7} item className={classes.gridImage}>
                     <img
                       src={poster}
                       alt={movieResult.title}
                       className={classes.image}
                     />
+                    <CircularProgress
+                      variant="determinate"
+                      color="secondary"
+                      className={classes.circular}
+                      value={movieResult.vote_average * 10}
+                    />
+                    <Typography
+                      variant="subtitle2"
+                      className={classes.circularText}
+                    >
+                      {movieResult.vote_average}
+                    </Typography>
+                    <IconButton
+                      color='primary'
+                      size='medium'
+                      aria-label="close drawer"
+                      onClick={props.handleClose}
+                      className={classes.closeIcon}
+                    >
+                      <CancelIcon />
+                    </IconButton>
                   </Grid>
                   <Grid
                     item
@@ -70,27 +106,108 @@ const MovieDetail = (props) => {
                     container
                     className={classes.gridRow}
                   >
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs className={classes.info}>
-                        <Typography
-                          gutterBottom
-                          variant="h5"
-                          color="primary"
-                          className={classes.title}
-                        >
-                          {movieResult.title}
-                        </Typography>
-                        <Typography variant="body2" className={classes.overview} gutterBottom>
-                          {movieResult.overview}
-                        </Typography>
-                      </Grid>
-                      <Grid item>
-                        <Typography
-                          variant="body2"
-                          style={{ cursor: "pointer" }}
-                        >
-                          Remove
-                        </Typography>
+                    <Grid item xs container direction="column">
+                      <Grid item xs>
+                        <Card className={classes.info}>
+                          <CardHeader
+                            className={classes.cardHeader}
+                            title={
+                              <Typography
+                                gutterBottom
+                                variant="h6"
+                                color="primary"
+                              >
+                                {movieResult.title}
+                              </Typography>
+                            }
+                            subheader={movieResult.tagline}
+                          />
+                          <CardContent>
+                            <Typography
+                              variant="body2"
+                              className={classes.overview}
+                              gutterBottom
+                            >
+                              <>
+                                <strong>Release date: </strong>
+                                {movieResult.release_date}
+                              </>
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              paragraph
+                              className={classes.overview}
+                              gutterBottom
+                            >
+                              <>
+                                <strong>Overview: </strong>
+                                {movieResult.overview}
+                              </>
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className={classes.overview}
+                              gutterBottom
+                            >
+                              <>
+                                <strong>Runtime: </strong>
+                                {movieResult.runtime} min
+                              </>
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className={classes.overview}
+                              gutterBottom
+                            >
+                              <>
+                                <strong>Budget: </strong>
+                                {new Intl.NumberFormat("de-DE", {
+                                  style: "currency",
+                                  currency: "USD",
+                                }).format(movieResult.budget)}
+                              </>
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className={classes.overview}
+                              gutterBottom
+                            >
+                              <>
+                                <strong>Revenue: </strong>
+                                {new Intl.NumberFormat("de-DE", {
+                                  style: "currency",
+                                  currency: "USD",
+                                }).format(movieResult.revenue)}
+                              </>
+                            </Typography>
+                            <Grid item className={classes.gridGenres}>
+                              <List className={classes.genres}>
+                                {movieResult.genres.map((genre) => {
+                                  return (
+                                    <ListItem>
+                                      <Typography
+                                        variant="body2"
+                                        color="primary"
+                                      >
+                                        {genre.name}
+                                      </Typography>
+                                    </ListItem>
+                                  );
+                                })}
+                              </List>
+                            </Grid>
+                          </CardContent>
+                          <CardActions className={classes.cardActions}>
+                            <Button
+                              size="small"
+                              color="primary"
+                              disabled={movieResult.homepage === ""}
+                              onClick={handleClick}
+                            >
+                              Visit the web page
+                            </Button>
+                          </CardActions>
+                        </Card>
                       </Grid>
                     </Grid>
                   </Grid>
--- src/pages/MovieDetail/style.jsx
+++ src/pages/MovieDetail/style.jsx
@@ -2,10 +2,12 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up("sm")]: {
       padding: theme.spacing(0),
       margin: "auto",
+      overflowY: "hidden",
     },
+    maxHeight: "90vh",
     overflowY: "scroll",
     flexWrap: "nowrap",
     "&::-webkit-scrollbar": {
@@ -18,28 +20,48 @@ const useStyles = makeStyles((theme) => ({
     "&::-webkit-scrollbar-thumb": {
       backgroundColor: "#3f51b5",
     },
-    maxHeight: "95vh",
-    padding: theme.spacing(0.5),
   },
-  root: {
-    [theme.breakpoints.up("md")]: {
-      width: "80vh",
+  principalGrid:{
+    [theme.breakpoints.up("sm")]: {
+      width: "45vw",
     },
-    paddingBlock: theme.spacing(5),
-    height: "100vh",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+  },
+  root: {
+    [theme.breakpoints.up("sm")]: {
+      width: "620px",
+    },
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
   },
   image: {
     height: "100%",
     width: "100%",
   },
-  info: {},
-  title: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  info: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+    maxHeight: '88.5vh',
+    [theme.breakpoints.up("sm")]: {
+      overflowY: "scroll",
+      flexWrap: "nowrap",
+      "&::-webkit-scrollbar": {
+        width: "0.3em",
+        height: "0.6em",
+      },
+      "&::-webkit-scrollbar-track": {
+        background: "#f0efff",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "#3f51b5",
+      },
+    },
   },
   overview: {
     textAlign: 'justify'
@@ -47,6 +69,51 @@ const useStyles = makeStyles((theme) => ({
   gridRow: {
     padding: theme.spacing(1),
   },
+  gridImage:{
+    position: 'relative',
+    display: 'block',
+    maxHeight: '90vh',
+  },
+  circular:{
+    position:'absolute',
+    bottom: 20,
+    right: 10
+  },
+  circularText:{
+    position:'absolute',
+    bottom: 28,
+    right: 20,
+    color: 'white'
+  },
+  genres:{
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  gridGenres:{
+    width: '100%',
+    flexWrap: "nowrap",
+    "&::-webkit-scrollbar": {
+      width: "0.3em",
+      height: "0.6em",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "#f0efff",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#3f51b5",
+    },
+    overflowX: "scroll",
+  },
+  cardActions:{
+    display: 'flex',
+    flexDirection: 'row', 
+    alignSelf: 'flex-end'
+  },
+  closeIcon:{
+    position:'absolute',
+    top: 0,
+    left: 10
+  }
 }));
 
 export default useStyles;
